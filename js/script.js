68d95478906c6f63f6dd908244dc6ec2c08af86d
dariaboyko <73283727+dariaboyko@users.noreply.github.com>
2022-04-04 12:16:14 +0100

    btnMenu

--- js/script.js
+++ js/script.js
@@ -244,4 +244,16 @@ const filterProducts = (array) => {
     return onSaleProducts;
 }
 generateContent(filterProducts(items));
-console.log(filterProducts(items))
+const menu = document.getElementById("topNav");
+const showMenu = document.getElementById("btnShowMenu");
+const hideMenu = document.getElementById("btnHideMenu");
+showMenu.addEventListener("click",()=>{
+menu.style.display = "flex";
+showMenu.style.display="none";
+hideMenu.style.display="block";
+});
+hideMenu.addEventListener("click",()=>{
+menu.style.display = "none";
+showMenu.style.display="block";
+hideMenu.style.display="none";
+});
